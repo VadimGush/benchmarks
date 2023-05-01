494b52167bb4b65e969064a62847d302e3fcbaf8
Vadim Gush <mail@vadimgush.com>
2023-05-02 01:33:45 +0400

    Performance improvement

--- print.py
+++ print.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
 import numpy as np
 
 df = pd.read_csv("result.csv", comment='#')
@@ -14,7 +13,6 @@ ax.plot(np.asarray(x, float), y)
 ax.set_xlabel("step (N)")
 ax.set_ylabel("time (microseconds)")
 ax.grid(True)
-# ax.xaxis.set_major_locator(ticker.MultipleLocator(10))
 ax.set_title("Update every Nth byte of an array")
 
 plt.show()
--- source/step.cpp
+++ source/step.cpp
@@ -35,41 +35,6 @@ f64 test(std::vector<u8>& data, const u32 step, const u32 total) {
 }
 
 constexpr u32 total_bytes_read = 1 * MB;
-std::vector<u8> garbage(50 * MB);
-
-void pollute_cache() {
-    for (u32 i = 0; i < garbage.size(); i++) { garbage[i] += i; }
-    u32 sum = 0;
-    for (unsigned char i : garbage) { sum += i; }
-    std::cout << "# " << sum << std::endl;
-}
-
-f64 benchmark(const u32 step) {
-    /*
-     * We will allocate some data in memory and try to read it. Because
-     * we want to measure how much time it takes to access elements of the array
-     * which is not in CPU cache currently, we will try to pollute cache after array
-     * of data will be created.
-     */
-
-    // Allocate data that we will read
-    std::vector<u8> data(total_bytes_read * step);
-    for (u32 i = 0; i < data.size(); i++) { data[i] += i; }
-
-    // Pollute cache by process some another array which size
-    // is larger than size of CPU cache.
-    pollute_cache();
-
-    // Perform reads with a given step
-    const f64 test_result = test(data, step, total_bytes_read);
-
-    // This is probably not needed. I've just added it here for the compiler
-    // to not remove everything related to the vector<u8> data, because otherwise
-    // it will not be used anywhere and can be eliminated from the binary completely.
-    std::cout << "# " << *std::min_element(data.begin(), data.end()) << std::endl;
-
-    return test_result;
-}
 
 struct test_result {
     std::vector<f64> time{};
@@ -82,13 +47,19 @@ int main() {
     constexpr u32 number_of_runs_per_test = 5;
     std::vector<test_result> test_results(256);
 
+    // Pre-allocate data
+    std::vector<u8> data(test_results.size() * total_bytes_read);
+    for (u32 i = 0; i < data.size(); i++) { data[i] += i; }
+
     // Execute benchmarks (tests)
-    for (u32 run = 0; run < number_of_runs_per_test; run++) {
-        for (u32 test = 0; test < test_results.size(); test++) {
-            const u32 step = test + 1;
-            const f64 execution_time = benchmark(step);
-            test_results[test].time.emplace_back(execution_time);
-            test_results[test].step = step;
+    for (u32 run_id = 0; run_id < number_of_runs_per_test; run_id++) {
+        for (u32 test_id = 0; test_id < test_results.size(); test_id++) {
+            const u32 step = test_id + 1;
+
+            const f64 execution_time = test(data, step, total_bytes_read);
+
+            test_results[test_id].time.emplace_back(execution_time);
+            test_results[test_id].step = step;
         }
     }
 
@@ -98,7 +69,7 @@ int main() {
     for (const auto& test_result : test_results) {
         // we will select the fastest execution time
         const f64 execution_time = *std::min_element(test_result.time.begin(), test_result.time.end());
-        std::cout << test_result.step << "," << execution_time << std::endl;
+        std::cout << test_result.step << "," << execution_time << '\n';
     }
 
     return 0;
